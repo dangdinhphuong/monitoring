5755478b44dcdaff67a1cccd462ffd3c137bee6a
phuongdd <dangxuong2000@gmail.com>
2023-11-27 21:44:22 +0700

    file

--- app/Http/Controllers/Admin/DashboadContrller.php
+++ app/Http/Controllers/Admin/DashboadContrller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ChanelController;
+use Illuminate\Support\Facades\File;
 
 class DashboadContrller extends Controller
 {
@@ -15,5 +16,17 @@ class DashboadContrller extends Controller
     public function index(){
         $channels = $this->chanelController->getChannelActive();
         return view('admin.pages.Home', compact('channels'));
+    }
+    public function hienThiTenTepTrongPublic() {
+        // Đường dẫn đến thư mục "public"
+        $publicPath = public_path();
+    
+        // Lấy danh sách tất cả các tệp trong thư mục "public"
+        $files = File::allFiles($publicPath);
+    
+        // Hiển thị tên của từng tệp
+        foreach ($files as $file) {
+            echo $file->getRelativePathname() . "<br>";
+        }
     }
 }
