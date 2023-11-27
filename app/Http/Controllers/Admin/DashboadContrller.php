a95faead953295711038acdc07e241ef359e4a90
phuongdd <dangxuong2000@gmail.com>
2023-11-27 21:54:00 +0700

    ghg

--- app/Http/Controllers/Admin/DashboadContrller.php
+++ app/Http/Controllers/Admin/DashboadContrller.php
@@ -14,20 +14,31 @@ class DashboadContrller extends Controller
         $this->chanelController = $chanelController;
     }
     public function index(){
-        dd($this->hienThiTenTepTrongPublic());
+        $this->hienThiTenTepTrongPublic();
+        dd($this->hienThiTenTepTrongPublic('storage/app/public'));
         $channels = $this->chanelController->getChannelActive();
         return view('admin.pages.Home', compact('channels'));
     }
-    public function hienThiTenTepTrongPublic() {
-        // Đường dẫn đến thư mục "public"
-        $publicPath = public_path();
-    
-        // Lấy danh sách tất cả các tệp trong thư mục "public"
-        $files = File::allFiles($publicPath);
-    
-        // Hiển thị tên của từng tệp
-        foreach ($files as $file) {
-            echo $file->getRelativePathname() . "<br>";
+    public function hienThiTenTepTrongPublic($folderPath = 'storage') {
+    // Kiểm tra xem đường dẫn thư mục có tồn tại không
+    if (!is_dir($folderPath)) {
+        return "Thư mục không tồn tại.";
+    }
+
+    // Lấy danh sách tất cả các tệp trong thư mục
+    $files = scandir($folderPath);
+
+    // Lọc ra các tệp thực sự bằng cách loại bỏ "." và ".."
+    $filteredFiles = array_diff($files, array('.', '..'));
+
+    // Hiển thị tên các tệp
+    if (count($filteredFiles) > 0) {
+        echo "Danh sách các tệp trong thư mục '$folderPath':<br>";
+        foreach ($filteredFiles as $file) {
+            echo "$file<br>";
         }
+    } else {
+        echo "Thư mục '$folderPath' không chứa tệp nào.";
+    }
     }
 }
